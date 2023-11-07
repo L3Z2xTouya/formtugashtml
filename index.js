bfc3f8c953c304c45f37fba6755c38516c2da99a
Touya06 <118948852+L3Z2xTouya@users.noreply.github.com>
2023-11-07 08:56:08 +0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function login(){
+    let user = document.getElementById("user").value;
+    let password = document.getElementById("password").value;
+
+    if (user == 'admin' && password == 'admin'){
+        alert("Selamat anda berhasil login!");
+        window.location = "https://l3z2xtouya.github.io/formtugashtml/portofolio/";
+    } else{
+        alert("User / password yang ddimasukkan salah!");
+    }
+}
